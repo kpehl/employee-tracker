4d0fe807e1987a145372cde6ce06857fea46f011
Kathleen P <30782087+kpehl@users.noreply.github.com>
2020-10-19 19:35:26 -0400

    added employees table to schema.js and sample data per the mock-up to seeds.js; database written; all tables verified on mysql command line

--- db/schema.sql
+++ db/schema.sql
@@ -17,3 +17,12 @@ CREATE TABLE roles(
     department_id INT NOT NULL,
     PRIMARY KEY (id)
 );
+
+CREATE TABLE employees(
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY (id)
+)
--- db/seeds.sql
+++ db/seeds.sql
@@ -14,3 +14,13 @@ VALUES
     ("Account", 125000, 3),
     ("Legal Team Lead", 250000, 4),
     ("Lawyer", 190000, 4);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES
+    ("John", "Doe", 1, 3),
+    ("Mike", "Chan", 2, 1),
+    ("Ashley", "Rodriguez", 3, NULL),
+    ("Kevin", "Tupik", 4, 3),
+    ("Malia", "Brown", 5, NULL),
+    ("Sarah", "Lourd", 6, NULL),
+    ("Tom", "Allen", 7, 6);
