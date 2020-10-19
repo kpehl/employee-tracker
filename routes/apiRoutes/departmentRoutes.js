e29024f96a884f6c474e8366f45927b11bc33bfa
Kathleen P <30782087+kpehl@users.noreply.github.com>
2020-10-19 15:29:03 -0400

    added api routes for getting a single department's information by id, for deleting a department, and for adding a department; verified all routes are functional with Insomnia Core

--- routes/apiRoutes/departmentRoutes.js
+++ routes/apiRoutes/departmentRoutes.js
@@ -21,4 +21,55 @@ router.get('/departments', (req, res) => {
     // res.json({ message: 'departments api'});
 });
 
+// An Express route to get a single department by id
+router.get('/department/:id', (req, res) => {
+    const sql = `SELECT * from departments
+                WHERE departments.id = ?`;
+    const params = [req.params.id];
+    connection.query(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
+// An Express route to delete a department
+router.delete('/department/:id', (req, res) => {
+    const sql = `DELETE FROM departments WHERE id = ?`;
+    const params = [req.params.id];
+    connection.query(sql, params, function(err, result) {
+        if (err) {
+            res.status(400).json ({ error: res.message});
+            return
+        }
+        res.json({
+            message: 'successfully deleted',
+            changes: this.changes
+        });
+    });
+});
+
+// An Express route to add a department
+router.post('/department', ({ body }, res) => {
+    const sql = `INSERT INTO departments (name)
+                VALUES (?)`;
+    const params = [body.name];
+    connection.query(sql, params, function(err, result) {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body,
+            id: this.lastID
+        });
+    });
+});
+
 module.exports = router;
