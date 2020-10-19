63a3dd89b97dfa8ddac6834daafecb97e880b5c6
Kathleen P <30782087+kpehl@users.noreply.github.com>
2020-10-19 19:03:13 -0400

    added route to add a new role; verified route with Insomnia Core

--- routes/apiRoutes/roleRoutes.js
+++ routes/apiRoutes/roleRoutes.js
@@ -43,4 +43,29 @@ router.get('/roles', (req, res) => {
     });
 });
 
+// An Express route to add a role
+router.post('/role', ({ body }, res) => {
+    // check the input for errors, and if there are any, return a 400 error to the client
+    const errors = inputCheck(body, 'title', 'salary', 'department_id');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+    // if no errors are found, proceed with the SQL route to insert a row
+    const sql = `INSERT INTO roles (title, salary, department_id)
+                VALUES (?, ?, ?)`;
+    const params = [body.title, body.salary, body.department_id];
+    connection.query(sql, params, function(err, result) {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body,
+            id: this.lastID
+        });
+    });
+});
+
 module.exports = router;
