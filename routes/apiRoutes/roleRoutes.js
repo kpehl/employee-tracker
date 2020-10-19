c8a16887a44976a3b7cbb842e3676a6005866611
Kathleen P <30782087+kpehl@users.noreply.github.com>
2020-10-19 18:52:18 -0400

    added a route to provide the role title, role id, department name, and role salary in the roleRoutes.js file; verified that route provides requested data with Insomnia Core

--- routes/apiRoutes/roleRoutes.js
+++ routes/apiRoutes/roleRoutes.js
@@ -5,8 +5,8 @@ const router = express.Router();
 const connection = require('../../db/database');
 const inputCheck = require('../../utils/inputCheck');
 
-// An Express route to return all the data in the departments table
-router.get('/roles', (req, res) => {
+// An Express route to return all the raw data in the roles table
+router.get('/rawroles', (req, res) => {
     const sql = `SELECT * FROM roles`;
     const params = [];
     connection.query(sql, params, (err, rows) => {
@@ -19,7 +19,28 @@ router.get('/roles', (req, res) => {
             data: rows
         });
     });
-    // res.json({ message: 'departments api'});
+});
+
+// An Express route to return data in the roles table joined with the department table
+router.get('/roles', (req, res) => {
+    const sql = `SELECT roles.title AS title,
+                roles.id AS role_id,
+                departments.name AS department_name,
+                roles.salary AS salary
+                FROM roles
+                LEFT JOIN departments
+                ON roles.department_id = departments.id`;
+    const params = [];
+    connection.query(sql, params, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message});
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
 });
 
 module.exports = router;
